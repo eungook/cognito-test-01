2494bb270e460b2cc3adaf1cb061a9af570c396d
YI Eun Gook <eungook83@gmail.com>
2021-03-12 16:45:32 +0900

    환경변수 추가

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
 export default {
-  region: '',
-  IdentityPoolId: '',
+  region: process.env.REACT_APP_REGION,
+  IdentityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID,
   UserPoolId: process.env.REACT_APP_USER_POOL_ID,
   ClientId: process.env.REACT_APP_CLIENT_ID,
 }
